adffa2e58819160c395e942201455a881d527f74
JsonCC <47939483+jsoncc@users.noreply.github.com>
2026-03-31 17:38:48 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/today-history/'  // 对应你的 GitHub 仓库名
+  base: '/today-history/', // 对应你的 GitHub 仓库名
+  publicDir: 'public'      // 确保 public 目录的 md 文件可访问
 })
